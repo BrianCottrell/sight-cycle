2daab520255cfa87a3b5777efba2fad9df2fc883
BrianCottrell <Brian_Cottrell@inbox.com>
2015-03-21 16:31:22 -0700

    Added blank node view.

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
+/* Smart or Bike      */
+/* by Brian Cottrell  */
+/* 03-21-2015         */
+
+//Add neccessary packages
+var express     = require('express');
+var app         = express();
+var mongoose    = require('mongoose');
+var bodyParser  = require('body-parser');
+var traitify    = require('traitify');
+var path        = require('path');
+
 var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World \n');
-}).listen(port);
+var port = process.env.PORT || 8080;
+
+//Configure body body-parser
+app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.json());
+
+//Configure view engine and directory path
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/views')));
+
+// Viewed at http://localhost:8080
+app.get('/', function(req, res){
+	//Displays the specified file on a webpage
+    res.sendFile(path.join(__dirname+'views/index.html.ejs'));
+});
+
+//Start the server
+app.listen(port);
+console.log('Listening to port:', port);
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World \n');
+// }).listen(port);
