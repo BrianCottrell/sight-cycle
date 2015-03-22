a962f45c3bd861209f91ec37772ee810ebb7c272
BrianCottrell <Brian_Cottrell@inbox.com>
2015-03-21 23:23:34 -0700

    Added connections to m2x data and home view.

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ var express     = require('express');
 var app         = express();
 var mongoose    = require('mongoose');
 var bodyParser  = require('body-parser');
-var traitify    = require('traitify');
 var path        = require('path');
 
 var http = require('http')
 var port = process.env.PORT || 8080;
+
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('m2xKey.json', 'utf8'));
+console.log(obj);
 
 //Configure body body-parser
 app.use(bodyParser.urlencoded({extend:true}));
@@ -19,12 +22,37 @@ app.use(bodyParser.json());
 
 //Configure view engine and directory path
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '/views')));
+//app.use(express.static(path.join(__dirname, '/views')));
 
-// Viewed at http://localhost:8080
+//Configure M2X and check status
+var M2X = require("m2x");
+var m2x = new M2X(obj.apiKey);
+// m2x.status(function(status) {
+//     console.log(status);
+// });
+var deviceData = [];
+
+m2x.devices.list(function(response) {
+    if (response.isSuccess()) {
+        response.json.devices.forEach(function(device) {
+            deviceData.push(device);
+            console.log(device);
+        });
+    } else {
+        console.log(response.error());
+    }
+});
+
+//Root view
 app.get('/', function(req, res){
-	//Displays the specified file on a webpage
-    res.sendFile(path.join(__dirname+'views/index.html.ejs'));
+    res.render('index.html.ejs', {
+        deviceData: deviceData,
+    });
+});
+
+//About view
+app.get('/about', function(req, res){
+    res.render('about.html.ejs');
 });
 
 //Start the server
