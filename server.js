d307243ade3d454bf4f5dd5282563f6e0f1e62e6
BrianCottrell <Brian_Cottrell@inbox.com>
2015-03-22 17:05:23 -0700

    Project somewhat ready to demo.

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var port = process.env.PORT || 8080;
 
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('m2xKey.json', 'utf8'));
-console.log(obj);
 
 //Configure body body-parser
 app.use(bodyParser.urlencoded({extend:true}));
@@ -31,22 +30,41 @@ var m2x = new M2X(obj.apiKey);
 //     console.log(status);
 // });
 var deviceData = [];
-
+var formattedDeviceData = [];
 m2x.devices.list(function(response) {
     if (response.isSuccess()) {
         response.json.devices.forEach(function(device) {
-            deviceData.push(device);
-            console.log(device);
+//            deviceData.push(device);
         });
+   //     console.log(deviceData);
     } else {
         console.log(response.error());
     }
 });
 
+m2x.devices.view("933e760c444999d84ca9c7980bc5831c", function(device) {
+    //console.log(device);
+}, function(error) { console.log(error); });
+
+m2x.devices.streamValues("933e760c444999d84ca9c7980bc5831c", "events", {}, function(stream){
+    deviceData = (stream.json.values);
+    //Format device data
+    for(var i = 0; i < deviceData.length; i++){
+        var newData = {};
+        newData.time = deviceData[i].timestamp;
+        newData.event = parseInt(deviceData[i].value.slice(0,2));
+        var v = deviceData[i].value;
+        newData.latitude = v.slice(v.indexOf(',')+1,v.substring(v.indexOf(',')+1, v.length).indexOf(',')+3);
+        newData.longitude = v.slice(v.substring(v.indexOf(',')+1, v.length).indexOf(',')+4, v.length);
+        formattedDeviceData.push(newData);
+    }
+    console.log(formattedDeviceData);
+},function(error) { console.log(error); });
+
 //Root view
 app.get('/', function(req, res){
     res.render('index.html.ejs', {
-        deviceData: deviceData,
+        deviceData: formattedDeviceData,
     });
 });
 
