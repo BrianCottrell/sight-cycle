62e3cbd3e3ab9054788e490d1f89d3cb8702e9a4
Joshua Herman <me@joshuaherman.org>
2015-08-07 23:08:48 -0700

    change to add server socket

--- edison/blue.py
+++ edison/blue.py
@@ -1,20 +1,107 @@
-# file: inquiry.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: performs a simple device inquiry followed by a remote name request of
-#       each discovered device
-# $Id: inquiry.py 401 2006-05-05 19:07:48Z albert $
-#
+import os
+import glob
+import time
+import mraa
+from bluetooth import *
 
-import bluetooth
+os.system(hciconfig hci0 name 'Intel Edison') #[change your device name to something else you fancy]
+wait(200)
+os.system(hciconfig hci0 piscan)
+#os.system('modprobe w1-gpio')
+#os.system('modprobe w1-therm')
 
-print("performing inquiry...")
 
-nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
+###########################################
 
-print("found %d devices" % len(nearby_devices))
 
-for addr, name in nearby_devices:
+
+print (mraa.getVersion())
+GPIO = mraa.Gpio(13)
+GPIO.dir(mraa.DIR_IN)
+input = GPIO.read()
+print (input)
+
+
+
+
+###########################################
+
+
+
+# base_dir = '/sys/bus/w1/devices/'
+# device_folder = glob.glob(base_dir + '28*')[0]
+# device_file = device_folder + '/w1_slave'
+
+# def read_temp_raw():
+#     f = open(device_file, 'r')
+#     lines = f.readlines()
+#     f.close()
+#     return lines
+
+# def read_temp():
+#     lines = read_temp_raw()
+#     while lines[0].strip()[-3:] != 'YES':
+#         time.sleep(0.2)
+#         lines = read_temp_raw()
+#     equals_pos = lines[1].find('t=')
+#     if equals_pos != -1:
+#         temp_string = lines[1][equals_pos+2:]
+#         temp_c = float(temp_string) / 1000.0
+#         temp_f = temp_c * 9.0 / 5.0 + 32.0
+#         return temp_c
+    
+#while True:
+#   print(read_temp())  
+#   time.sleep(1)
+
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "TestServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+#                   protocols = [ OBEX_UUID ] 
+                    )
+while True:          
+    print "Waiting for connection on RFCOMM channel %d" % port
+
+    client_sock, client_info = server_sock.accept()
+    print "Accepted connection from ", client_info
+
     try:
-        print("  %s - %s" % (addr, name))
-    except UnicodeEncodeError:
-        print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
+            data = client_sock.recv(1024)
+            if len(data) == 0: break
+            print "received [%s]" % data
+
+        if data == 'temp':
+            data = str(read_temp())+'!'
+        elif data == 'lightOn':
+            GPIO.output(17,False)
+            data = 'light on!'
+        elif data == 'lightOff':
+            GPIO.output(17,True)
+            data = 'light off!'
+        else:
+            data = 'WTF!' 
+            client_sock.send(data)
+        print "sending [%s]" % data
+
+    except IOError:
+        pass
+
+    except KeyboardInterrupt:
+
+        print "disconnected"
+
+        client_sock.close()
+        server_sock.close()
+        print "all done"
+
+        break
