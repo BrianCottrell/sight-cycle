f85943b3b77adbc6f7cda7cd7c71e7482b3821c7
Joshua Herman <me@joshuaherman.org>
2015-08-10 20:07:52 -0700

    testing hall effect

---
+++ edison/hallsensor.py
@@ -0,0 +1,31 @@
+import mraa
+from datetime import datetime
+
+try:
+	print (mraa.getVersion())
+except ImportError:
+	print("Mraa failed to import")
+	quit()
+
+
+print (mraa.getVersion())
+x = mraa.Gpio(13)
+x.dir(mraa.DIR_IN)
+
+startTime = datetime.now()
+
+
+bool Timeout = True
+
+while Timeout==True:
+	currentTime = datetime.now()
+	delta = currentTime - startTime
+	
+	
+	if (x.read(1) == 1):
+		count++
+		print("Rotation" count)
+
+	else if (delta.total_seconds() )== 5000):
+		quit()
+
